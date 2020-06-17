29919a0a39ab7f8383acf5f1fa6b98d971f83b0a
Clark Farriss <clarkfarriss@clarks-mbp.home>
2020-06-16 23:56:42 -0400

    added some logic

--- Javascript/logic.js
+++ Javascript/logic.js
@@ -1,4 +1,20 @@
 // index logic here
+var xBtnNavIndex = document.getElementsByClassName(".x-nav");
+var indexNav = document.getElementById("indexNav");
+var openNav = document.getElementById("openNav");
+var sideNavWhole = document.getElementById("sideNav");
+
+function closeNav(){
+    indexNav.style.display = "none";
+    openNav.style.display = "initial";
+    sideNavWhole.style.display = "none";
+}
+
+function openNavFunc(){
+    indexNav.style.display = "initial";
+    openNav.style.display = "none";
+    sideNavWhole.style.backgroundColor = "#fff";
+}
 
 //
 
